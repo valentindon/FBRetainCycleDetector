08351666ee5cb808855e64cc9e4af69ae77c26c8
val <valentindon@users.noreply.github.com>
2024-08-20 11:44:07 +0300

    Fix

---
+++ FBRetainCycleDetector/FBRetainCycleDetector-Swift.h
@@ -0,0 +1,13 @@
+//
+//  FBRetainCycleDetector-Swift.h
+//  FBRetainCycleDetector
+//
+//  Created by Dev on 20.08.2024.
+//  Copyright © 2024 Facebook. All rights reserved.
+//
+
+#ifndef FBRetainCycleDetector_Swift_h
+#define FBRetainCycleDetector_Swift_h
+
+
+#endif /* FBRetainCycleDetector_Swift_h */
